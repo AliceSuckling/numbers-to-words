fc80475b9931830d034be82ea02dc26d88ecb83e
Alice Suckling <alicesuckling@live.co.uk>
2016-04-20 20:08:20 +0100

    a few changes

--- dist/scripts/controllers/NumberCtrl.js
+++ dist/scripts/controllers/NumberCtrl.js
@@ -1,40 +1,40 @@
 (function() {
   function NumberCtrl($scope) {
     ctrl = this;
-    
-    var small = new Array(' ', ' one', ' two', ' three', ' four', ' five', ' six', ' seven', ' eight', ' nine', ' ten', ' eleven', ' twelve', ' thirteen', ' fourteen', ' fifteen', ' sixteen', ' seventeen', ' eighteen', ' nineteen');
-    var medium = new Array(' ',' ', ' twenty', ' thirty', ' forty', ' fifty', ' sixty', ' seventy', ' eighty', ' ninety');
+
+    var small = new Array(' ', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', ' ten', ' eleven', ' twelve', ' thirteen', ' fourteen', ' fifteen', ' sixteen', ' seventeen', ' eighteen', ' nineteen');
+    var medium = new Array(' ',' ', ' twenty-', ' thirty-', ' forty-', ' fifty-', ' sixty-', ' seventy-', ' eighty-', ' ninety-');
     var hundred = ' hundred';
-    
+
     var translateTwoDigits = function(num) {
       var spaced = ' ';
       var stringOfNumbers = num.toString();
-      
+
       if (num < 100 && num > 19) {
         spaced += medium[parseInt(stringOfNumbers.charAt(0))];
         spaced += small[parseInt(stringOfNumbers.charAt(1))];
       } else {
         spaced = small[num];
       }
-      
+
       return spaced;
     }
-    
+
     var translate = function(num) {
       var spaced = ' ';
       var stringOfNumbers = num.toString();
 
       if (stringOfNumbers.length == 3) {
-        spaced = small[parseInt(stringOfNumbers.charAt(0))] + hundred + ' and';
+        spaced = small[parseInt(stringOfNumbers.charAt(0))] + hundred + ' and ';
         var twoDigitNumber = parseInt(stringOfNumbers.charAt(1) + stringOfNumbers.charAt(2));
         spaced += translateTwoDigits(twoDigitNumber);
       } else {
         spaced = translateTwoDigits(num);
       }
-      
+
       return spaced;
     }
-    
+
     var process = function(stringOfNumbers) {
     // special cases
       if (stringOfNumbers == '0') {
@@ -55,13 +55,13 @@
 
       return translate(parseInt(stringOfNumbers));
     }
-    
+
     ctrl.convert = function() {
       var stringOfNumbers = $scope.input;
       $scope.output = process(stringOfNumbers);
     }
   }
-  
+
   angular
     .module('numbersToWords')
     .controller('NumberCtrl', ['$scope', NumberCtrl]);
